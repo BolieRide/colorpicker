65d8d7eefe1f424bb072885a9569909234916e5e
BolieRide <BolieRide@gmail.com>
2021-10-21 08:35:06 -0400

    Add import the colors from web

---
+++ import_colors_from_web.py
@@ -0,0 +1,34 @@
+# Importing the required modules
+import os
+import sys
+import lxml
+import pandas as pd
+from bs4 import BeautifulSoup
+
+input = "D:\PDM_Class\colorpicker\dakkadakka.html"
+
+with open(input, 'r') as f:
+    contents = f.read()
+
+# for getting the header from
+# the HTML file
+list_header = []
+soup = BeautifulSoup(contents, 'lxml')
+division = soup.find(id="header")
+for td in division.find_all("td"):
+    try:
+        shead = str(td.get_text())
+        shead2 = shead.replace("\n", "")
+        list_header.append(shead2)
+        print(list_header)
+    except:
+        continue
+
+# Storing the data into Pandas
+df = pd.read_html(input)
+df2 = df[-1]
+df2.reset_index(drop=True)
+df2.columns = list_header
+df2 = df2.iloc[1:,:]
+print(df2)
+df2.to_csv('dakka.csv')
