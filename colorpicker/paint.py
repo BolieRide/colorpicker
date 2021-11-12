fd7cd59eecd25fa500bdd890436b5d9eac9939c9
JnyJny <erik.oshaughnessy@gmail.com>
2021-11-12 11:55:18 -0600

    Added more functionality to the Paint class - fixed create_catalog to create lists of Paint instances - added create_product_catalog to create a dictionary keyed by brand - added distance method to Paint to calculate euclidean distance   between two colors - added distance_squared method that sums the squared differences   for performance-limited situtations

--- colorpicker/paint.py
+++ colorpicker/paint.py
@@ -1,32 +1,66 @@
 from .data import paint_data
 from pathlib import Path
 import csv
+import math
 from typing import Union
 
 
 class Paint:
-    """
+    """"""
 
-    """
     @classmethod
-    def create_catalog(cls, path: Union[Path, str] = None) -> dict:
+    def create_catalog(cls, path: Union[Path, str] = None) -> dict[str, list["Paint"]]:
+
         path = Path(path or paint_data)
         catalog = {}
-        brand = {}
         rows = list(csv.reader(path.open()))
-        header = rows[0]
+        brands = rows[0]
+
         for row in rows[1:]:
-            idx, *names = row
-            brand = dict((zip(header[1:], names)))
-            catalog.update({idx: brand})
+            color_int, *names = row
+            color_tuple = tuple(color_int.to_bytes(byteorder="big"))
+            color_str = str(hex(color_int))
+            for column, name in enumerate(names, 1):
+                if name == "-":
+                    continue
+                paint = cls(color_tuple, name, brands[row_number])
+                catalog.setdefault(color_str, []).append(paint)
         return catalog
 
+    @classmethod
+    def create_brand_catalog(
+        cls, path: Union[Path, str] = None
+    ) -> dict[str, list["Paint"]]:
 
+        catalog = cls.create_catalog(path)
 
-    def __init__(self, color: tuple[int, int, int], name: str, line: str):
+        brand_catalog = {}
+
+        for paints in catalog.values():
+            for paint in paints:
+                brand_catalog.setdefault(paint.brand, []).append(paint)
+
+        return brand_catalog
+
+    # dataclass
+    def __init__(self, color: tuple[int, int, int], name: str, brand: str) -> None:
         self.color = color
         self.name = name
-        self.line = line
+        self.brand = brand
 
-    def distance(self, other: tuple[int, int, int]):
-        pass
+    def distance(self, other: tuple[int, int, int]) -> float:
+        """Euclidean distance between self and other."""
+
+        # d([x,y,z], [a,b,c]) = sqrt( (x-a)**2 + (y-b)**2 + (z-c)**2)
+
+        return math.sqrt(self.distance_squared(other))
+
+    def distance_squared(self, other: tuple[int, int, int, int]) -> float:
+        """Sum of squared-differences between self and other."""
+
+        # sum = 0
+        # for a, b in zip(self.color, other):
+        #    sum += (a - b) ** 2
+        # return sum
+
+        return sum([(a - b) ** 2 for a, b in zip(self.color, other)])
